a0a29bc4cb503171a806175657c842f01d853b2b
adambursik <adam.bursik@skolakrizik.cz>
2026-04-27 08:25:27 +0200

    Add files via upload

---
+++ funkce.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int scitacni(int a , int b){
+return a+b;
+}
+int odcitani (int a , int b){
+return a-b;
+}
+int nasobeni(int a , int b){
+return a*b;
+}
+int deleni(int a , int b){
+return a/b;
+}
+
+
+int main() {
+  char pocet;
+  int x;
+  int y;
+  double vysldek;
+  cout<<"zadejte 2 èisla a znak. \n";
+  cout<<"1. cislo: \n";
+  cin>> x;
+  cout<<"2. cislo: \n";
+  cin>> y;
+  cout<<"Zadejte znak. (+, -, *, /): ";
+  cin>> pocet;
+
+  switch (pocet){
+
+  case '+':
+      int scitani (x, y);
+
+  case '-':
+
+
+  case '*':
+
+
+  case '/':
+
+
+  default:
+    cout<<"Neco mas blbe blbecku";
+  }
+  }
